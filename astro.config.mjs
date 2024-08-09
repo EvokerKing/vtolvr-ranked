f7c0a514ea12408e5cc9fe700f6509d2b978cc3f
Hex <maxwellturtle@icloud.com>
2024-08-09 15:37:04 -0400

    Add multipliers page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import node from "@astrojs/node"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'hybrid',
+	adapter: node({
+		mode: "standalone"
+	}),
+	experimental: {
+		serverIslands: true
+	},
+	server: { port: 8080 }
+});
